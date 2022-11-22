d36df457d360cec36fb4adeca4cf2606295784a0
CarlosGonzalez <carlogonzalez@alumno.unlam.edu.ar>
2022-11-22 14:57:33 -0300

    add header post

--- src/Hooks/usePostOrdern.js
+++ src/Hooks/usePostOrdern.js
@@ -2,6 +2,7 @@ import Axios from "axios";
 export async function generateOrden(data) {
   try {
     const response = await Axios({
+      headers: { "Content-Type": "application/json" },
       method: "POST",
       url: "http://localhost:3005/Pedidos/AltaPedido",
       data: data,
