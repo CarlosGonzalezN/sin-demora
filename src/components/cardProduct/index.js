3661addebf53f0274a7a2480e2bcf00f2587a724
CarlosGonzalez <carlogonzalez@alumno.unlam.edu.ar>
2022-11-22 14:07:29 -0300

    product image

--- src/components/cardProduct/index.js
+++ src/components/cardProduct/index.js
@@ -30,41 +30,47 @@ export default function Index({ products }) {
     <>
       <>
         {" "}
-        {data.map((data) => (
-          <Card sx={{ display: "flex", marginTop: 3 }} key={data.id}>
-            <Box sx={{ display: "flex", flexDirection: "column" }}>
-              <CardContent sx={{ flex: "1 0 auto" }}>
-                <Typography component="div" variant="h5">
-                  {data.Nombre}
-                </Typography>
-                <Typography
-                  variant="subtitle1"
-                  color="text.secondary"
-                  component="div"
+        {data.map((data) => {
+          const imgSrc = require(`../../images/productos/${data.Id}.png`);
+          return (
+            <Card sx={{ display: "flex", marginTop: 3 }} key={data.id}>
+              <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <CardContent sx={{ flex: "1 0 auto" }}>
+                  <Typography component="div" variant="h5">
+                    {data.Nombre}
+                  </Typography>
+                  <Typography
+                    variant="subtitle1"
+                    color="text.secondary"
+                    component="div"
+                  >
+                    {data.DescripcionCategoria}
+                  </Typography>
+                </CardContent>
+                <Box
+                  sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}
                 >
-                  {data.DescripcionCategoria}
-                </Typography>
-              </CardContent>
-              <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
-                <Typography component="div" variant="h4">
-                  ${data.Precio}
-                </Typography>
-                <CardActions>
-                  <Button size="small" onClick={() => handleOpen(data.Id)}>
-                    <AddShoppingCartIcon />
-                  </Button>
-                </CardActions>
+                  <Typography component="div" variant="h4">
+                    ${data.Precio}
+                  </Typography>
+                  <CardActions>
+                    <Button size="small" onClick={() => handleOpen(data.Id)}>
+                      <AddShoppingCartIcon />
+                    </Button>
+                  </CardActions>
+                </Box>
               </Box>
-            </Box>
-
-            <CardMedia
-              component="img"
-              sx={{ width: 129, height: 97, marginTop: 4 }}
-              image="/empanada.png"
-              alt="imagen del producto"
-            />
-          </Card>
-        ))}
+              {
+                <CardMedia
+                  component="img"
+                  sx={{ width: 129, height: 97, marginTop: 4 }}
+                  src={imgSrc}
+                  alt={`../../images/productos/${data.$id}.png`}
+                />
+              }
+            </Card>
+          );
+        })}
         <Modal
           open={openModal}
           onClose={handleClose}
