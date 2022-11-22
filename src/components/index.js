c267a89b00ac4ef327b66104875b5b2ddf210975
CarlosGonzalez <carlogonzalez@alumno.unlam.edu.ar>
2022-11-21 22:52:05 -0300

    delete components export default

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Nav } from "./navbar/index";
-export { default as Category } from "./category/index";
-export { default as CardProduct } from "./cardProduct/index";
-export { default as Products } from "./products/index";
-export { default as ModalProduct } from "./modalProduct/index";
-export { default as Orden } from "./orden/index";
