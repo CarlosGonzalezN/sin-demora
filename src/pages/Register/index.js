2c06ba10fc058eaa4e16c04c9f0f561187c48558
CarlosGonzalez <carlogonzalez@alumno.unlam.edu.ar>
2022-11-23 09:10:41 -0300

    register center

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -16,35 +16,33 @@ export default function Index() {
   };
 
   return (
-    <div className="container">
-      <div className="main">
-        <Box component="form" noValidate autoComplete="off" sx={{}}>
-          <div>
-            <Typography variant="h6" gutterBottom>
-              Registrese
-            </Typography>
-            <br />
+    <div className="register">
+      <Box component="form" noValidate autoComplete="off" sx={{}}>
+        <div>
+          <Typography variant="h6" gutterBottom>
+            Registrese
+          </Typography>
+          <br />
 
-            <TextField
-              id="outlined-basic"
-              label="Nombre"
-              variant="outlined"
-              name="nombre"
-              value={nombre}
-              onChange={handleChange}
-            />
-            <br />
-            <Button
-              size="small"
-              variant="contained"
-              sx={{ marginTop: 3, marginLeft: 15 }}
-              onClick={handleSumbit}
-            >
-              Registrar
-            </Button>
-          </div>
-        </Box>
-      </div>
+          <TextField
+            id="outlined-basic"
+            label="Nombre"
+            variant="outlined"
+            name="nombre"
+            value={nombre}
+            onChange={handleChange}
+          />
+          <br />
+          <Button
+            size="small"
+            variant="contained"
+            sx={{ marginTop: 3, marginLeft: 15 }}
+            onClick={handleSumbit}
+          >
+            Registrar
+          </Button>
+        </div>
+      </Box>
     </div>
   );
 }
