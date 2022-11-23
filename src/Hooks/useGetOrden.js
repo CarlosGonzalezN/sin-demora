b4f2a765f71ed525d413129873629a740036037c
CarlosGonzalez <carlogonzalez@alumno.unlam.edu.ar>
2022-11-22 21:35:37 -0300

    url

--- src/Hooks/useGetOrden.js
+++ src/Hooks/useGetOrden.js
@@ -2,11 +2,14 @@ import Axios from "axios";
 
 export async function getAllOrden(idMesa) {
   try {
-    const response = await Axios.get("http://localhost:44328/Pedidos/Alta", {
-      params: {
-        IdMesa: idMesa,
-      },
-    });
+    const response = await Axios.get(
+      "http://localhost:44328/Pedidos/DetallePedido",
+      {
+        params: {
+          IdMesa: idMesa,
+        },
+      }
+    );
     console.log(response);
     return response;
   } catch (error) {}
